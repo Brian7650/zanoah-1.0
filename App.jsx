57e2a551df0de0f8c627378aafaa2cb26f2f49fc
Brian7650 <brtl765@gmail.com>
2024-04-01 15:55:38 -0400

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className="text-blue-600/75 text-9xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
