fc6fe2fc437f57db56e6695423c04ef51e7d1485
Brian7650 <brtl765@gmail.com>
2024-04-30 01:56:54 -0400

    Added redux & redux-persist files. Need to troubleshoot and test

---
+++ redux/store.js
@@ -0,0 +1,26 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit' //combine reducers
+import userReducer from './user/userSlice'
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+const rootReducer = combineReducers({//combine reducers
+    user: userReducer,
+});
+
+const persistConfig = {//persist config
+    key: 'root',
+    storage,
+    version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);//persisted reducer
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false,// middleware to help prevent error using redux-persist
+  }),
+});
+
+export const persistor = persistStore(store); //persistor to persist the store
